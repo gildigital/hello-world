114b615c33a82175ae16409a0bf3c7b576a2788c
gildigital <105453328+gildigital@users.noreply.github.com>
2023-01-01 19:44:31 -0800

    Update hello-world.py
    Added a new user question.

--- hello-world.py
+++ hello-world.py
@@ -3,4 +3,9 @@ print("Hello.")
 name = input("What's your name? Enter name: ")
 
 print("Hello " + str(name) + ". You're cool.")
+
+major = input("What's your major? Enter major: ")
+
+print("Cool! " + major + " is awesome. Good luck with that.")
+
     
